65b3b11f872bbc9dcbe9f0fcb412f52c88d1b5c5
Richard Jérémy <jeremy.rc77@gmail.com>
2024-06-10 16:47:01 +0700

    s

--- sportsee-front/src/components/charts/ActivityChart.tsx
+++ sportsee-front/src/components/charts/ActivityChart.tsx
@@ -18,7 +18,9 @@ const ActivityChart = (props: ActivityChartProps) => {
   if (props.isLoading) return <Loading />;
   return (
     <div className="flex flex-col items-center justify-center bg-[#FBFBFB] shadow-sm rounded-md h-[320px]">
-      <p className="self-start pt-6 pb-16 pl-7 font-semibold">Activité quotidienne</p>
+      <p className="self-start pt-6 pb-16 pl-7 font-semibold">
+        Activité quotidienne
+      </p>
       <ResponsiveContainer width="100%" height={210}>
         <BarChart
           data={props.data}
@@ -29,18 +31,26 @@ const ActivityChart = (props: ActivityChartProps) => {
             bottom: 5,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
+          <CartesianGrid
+            strokeDasharray="3 3"
+            vertical={false}
+            verticalPoints={[100, 200]}
+          />
           <Tooltip />
-          <Legend wrapperStyle={{ top: -85, left: 360 }} />
+          <Legend wrapperStyle={{ top: -85, left: 300 }} iconType="circle" />
           <Bar
+            name="Poids (kg)"
             dataKey="kilogram"
-            fill="#E60000"
-            activeBar={<Rectangle fill="#E60000" stroke="blue" />}
+            fill="#282D30"
+            activeBar={<Rectangle fill="#282D30" />}
+            radius={[300, 300, 0, 0]}
           />
           <Bar
+            name="Calories brûlées (kCal)"
             dataKey="calories"
-            fill="#282D30"
-            activeBar={<Rectangle fill="#282D30" stroke="yellow" />}
+            fill="#E60000"
+            activeBar={<Rectangle fill="#E60000" />}
+            radius={[300, 60, 0, 0]}
           />
         </BarChart>
       </ResponsiveContainer>
--- sportsee-front/src/components/charts/SessionChart.tsx
+++ sportsee-front/src/components/charts/SessionChart.tsx
@@ -1,10 +1,22 @@
-import { LineChart, Line, ResponsiveContainer } from "recharts";
+import { LineChart, Line, ResponsiveContainer, Tooltip } from "recharts";
 import Loading from "../Utilities/Loading";
 
 interface SessionChartProps {
   data: Array<{ sessionLength: number }>;
   isLoading: boolean;
 }
+
+const SessionToolTip = ({ payload }) => {
+  if (payload && payload.length) {
+    return (
+      <div className="bg-white p-2">
+        <p>{payload[0].value + " min"}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
 
 const SessionChart = (props: SessionChartProps) => {
   if (props.isLoading) return <Loading />;
@@ -16,11 +28,14 @@ const SessionChart = (props: SessionChartProps) => {
       </p>
       <ResponsiveContainer width="100%" height="60%">
         <LineChart width={300} height={100} data={props.data}>
+          <Tooltip content={<SessionToolTip payload={[]} />} />
+
           <Line
             type="monotone"
             dataKey="sessionLength"
             stroke="#FFFFFF"
             strokeWidth={1}
+            activeDot={{ r: 6 }}
           />
         </LineChart>
       </ResponsiveContainer>
